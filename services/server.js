9057597c6d4f4628f3048d3efc39c3b206d90256
Matt Mokary <mxm6060@rit.edu>
2015-04-18 21:02:58 -0400

    Brutally getting rid of cross-origin problems

--- services/server.js
+++ services/server.js
@@ -41,6 +41,16 @@ router.use(function(req, res, next) {
     next();
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+router.use(allowCrossDomain);
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
